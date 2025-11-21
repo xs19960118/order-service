c8b5c159719b23b64bd40f5d6b3b6c963b87c05c
xs <1744110814@qq.com>
2025-11-21 11:45:19 +0800

    add

--- start/src/main/java/com/xs/order/service/start/StartApplication.java
+++ start/src/main/java/com/xs/order/service/start/StartApplication.java
@@ -5,16 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * @author xiangshang
+ */
 @SpringBootApplication(scanBasePackages = "com.xs.order.service")
 @MapperScan("com.xs.order.service.infrastructure.persistence.mapper")
 public class StartApplication {
+    private static final Logger logger = LoggerFactory.getLogger(StartApplication.class);
+    
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
 
     @Bean
     public org.springframework.boot.CommandLineRunner checkConfig(Environment env) {
-        return args -> System.out.println("Loaded spring.application.name = " + env.getProperty("spring.application.name"));
+        return args -> logger.info("Loaded spring.application.name = {}", env.getProperty("spring.application.name"));
     }
 }
