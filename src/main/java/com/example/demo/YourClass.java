a1924e4eb5600c4cad6edd2fefbd1b161a4bbad9
xs <1744110814@qq.com>
2025-12-12 17:47:58 +0800

    1

--- src/main/java/com/example/demo/YourClass.java
+++
@@ -1,5 +0,0 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class YourClass {
-    private static final Logger logger = LoggerFactory.getLogger(YourClass.class);
--- src/main/java/com/xs/order/service/infrastructure/config/Knife4jConfig.java
+++
@@ -1,5 +0,0 @@
-                } catch (Exception e) {
-                    logger.error("处理文档时发生错误: ", e);
-                }
-            };
-        }
